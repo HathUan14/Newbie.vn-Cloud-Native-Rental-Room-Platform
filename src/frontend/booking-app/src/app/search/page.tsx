bc7b54acbdcdc59e8625f5ed4583a7bc1247aff3
kozkhoa <khoahuynh787898@gmail.com>
2025-11-07 15:31:25 +0700

    Upload filter bar and update room info

--- src/frontend/booking-app/src/app/search/page.tsx
+++ src/frontend/booking-app/src/app/search/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import FilterBar from "@/components/FilterBar";
+import RoomInfo, { RoomProps } from "@/components/RoomInfo";
+
 function SearchPage() {
-  return <div>hello</div>;
+  const room: RoomProps = {
+    title: "titile",
+    price: 123,
+    guestCapacity: 2,
+    addressStreet: "24w",
+    ward: "2134",
+    district: "2345",
+    city: "1245",
+    imageUrl: "room-image-1.jpg",
+  };
+
+  const handleFilter = (filter = {}) => {
+    console.log(filter);
+  };
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <div className=" sticky">
+        <FilterBar
+          onFilter={(filter) => handleFilter(filter)}
+          className={`flex flex-col items-center justify-center `}
+        ></FilterBar>
+      </div>
+      <div>
+        <RoomInfo room={room}></RoomInfo>
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
