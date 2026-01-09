12ce0f75b8374aece518dd26238ab3d99881f192
HathUan14 <hazzthuan@gmail.com>
2026-01-10 02:55:02 +0700

    fix CORS error backend connect frontend

--- src/backend/booking-app/src/main.ts
+++ src/backend/booking-app/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000'],
+    origin: ['http://localhost:3001', 'http://localhost:3000', 'https://introduction-se.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
